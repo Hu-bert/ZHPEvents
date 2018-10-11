2953982651dd74aa3518890ec3184392e822355a
Hubert Grzesiak <hubert.grzesiak@outlook.com>
2018-10-11 21:55:21 +0200

    add register, login, logout and email sender

---
+++ ZHPEvents/Areas/Identity/Data/ZHPEventsUser.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZHPEvents.Models.Identity
+{
+    public class ZHPEventsUser : IdentityUser
+    {
+        [PersonalData]
+        public string FristName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+        [PersonalData]
+        public StopinieHarcerskie StopienHarcerski { get; set; }
+        [PersonalData]
+        public StopnieInstruktorskie StopienInstruktorski { get; set; }
+        [PersonalData]
+        public Hufce Hufiec { get; set; }
+        [PersonalData]
+        public Choragwie Choragiew { get; set; }
+    }
+
+    public enum StopinieHarcerskie
+    {
+        [Display(Name = "Nie dotyczy")]
+        none,
+        [Display(Name = "mł,")]
+        ml,
+        [Display(Name = "och.")]
+        och,
+        [Display(Name = "wyw.")]
+        wyw,
+        [Display(Name = "trop.")]
+        trop,
+        [Display(Name = "odkr.")]
+        odkr,
+        [Display(Name = "pion.")]
+        pion,
+        [Display(Name = "ćw.")]
+        ćw,
+        [Display(Name = "sam.")]
+        sam,
+        [Display(Name = "HO.")]
+        HO,
+        [Display(Name = "HR.")]
+        HR
+    }
+
+    public enum StopnieInstruktorskie
+    {
+        [Display(Name = "Nie dotyczy")]
+        none,
+        [Display(Name = "pwd.")]
+        pwd,
+        [Display(Name = "phm.")]
+        phm,
+        [Display(Name = "hm.")]
+        hm
+    }
+    public enum Choragwie
+    {
+        [Display(Name = "Nie dotyczy")]
+        none,
+        [Display(Name = "Białostocka")]
+        Bialostocka,
+        [Display(Name = "Dolnoslaska")]
+        Dolnośląska,
+        [Display(Name = "Gdanska")]
+        Gdanska,
+        [Display(Name = "Kielecka")]
+        Kielecka,
+        [Display(Name = "Krakowska")]
+        Krakowska,
+        [Display(Name = "Kujawsko Pomorska")]
+        KujawskoPomorska,
+        [Display(Name = "Lubelska")]
+        Lubelska,
+        [Display(Name = "Łódzka")]
+        Lodzka,
+        [Display(Name = "Mazowiecka")]
+        Mazowiecka,
+        [Display(Name = "Opolska")]
+        Opolska,
+        [Display(Name = "Podkarpacka")]
+        Podkarpacka,
+        [Display(Name = "Stołeczna")]
+        Stoleczna,
+        [Display(Name = "Śląska")]
+        Slaska,
+        [Display(Name = "Warmińsko Mazurska")]
+        WarminskoMazurska,
+        [Display(Name = "Wilekopolska")]
+        Wilekopolska,
+        [Display(Name = "Zachodnio Pomorska")]
+        ZachodnioPomorska,
+        [Display(Name = "Ziemi Lubuskiej")]
+        ZiemiLubuskiej
+    }
+    public enum Hufce
+    {
+        [Display(Name = "Nie dotyczy")]
+        none,
+        [Display(Name = "Oleśnica")]
+        Olesnica,
+        [Display(Name = "Wrocław")]
+        Wroclaw
+    }
+
+}
--- ZHPEvents/Startup.cs
+++ ZHPEvents/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.EntityFrameworkCore;
 using ZHPEvents.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ZHPEvents.Areas.Identity.Services;
 
 namespace ZHPEvents
 {
@@ -37,8 +39,46 @@ namespace ZHPEvents
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>(config =>
+                {
+                    config.SignIn.RequireConfirmedEmail = true;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
